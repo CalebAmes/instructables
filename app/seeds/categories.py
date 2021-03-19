cb57f9729e0275470e6f1295ce9ae4675f577fd2
Matt Johnston <emollientspy1@gmail.com>
2021-03-19 11:35:24 -0700

    categories seed data working

--- app/seeds/categories.py
+++ app/seeds/categories.py
@@ -3,13 +3,22 @@ from app.models import db, Category
 
 def seed_categories():
 
-    category1 = Category(name='Cooking', titles = ["Breakfast", "Lunch", "Dinner", "Banquet"],
-                imgs=["img-url1", "img-url2", "img-url3", "img-url4"], descriptions=["d1", "d2", "d3", "d4"])
+    category1 = Category(name="Cooking", titles=["asdfa", "asdkfj", "ajsdhf"], imgs=["adslgj", "skal", "fdsjakl"], descriptions=["ajksdfh", "dsalfkj", "afdjskl"])
+
+    # category1 = Category(name="Cooking")
+    # category1.titles = "title1"
+    # category1.titles = "title2"
+    # category1.titles = "title3"
+    # category1.imgs = "img-url1"
+    # category1.imgs = "img-url2"
+    # category1.imgs = "img-url3"
+    # category1.descriptions = "description1"
+    # category1.descriptions = "description2"
+    # category1.descriptions = "description3"
 
     db.session.add(category1)
 
     db.session.commit()
-
 
 def undo_categories():
     db.session.execute('TRUNCATE categories;')
