6c1a86500d2ecfbc364f12c50f7dad70b6729817
Caleb <caleb-gilbert@live.com>
2021-03-22 16:14:08 -0400

    root Reducer

---
+++ react-app/src/store/index.js
@@ -0,0 +1,22 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import project from './project'
+import comment from './comment'
+import category from './category'
+
+const rootReducer = combineReducers({
+  project,
+  comment,
+  category,
+})
+
+const logger = require('redux-logger').default;
+const composeEnhancers =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+
+const configureStore = (preloadedState) => {
+  return createStore(rootReducer, preloadedState, enhancer);
+};
+
+export default configureStore;
