15c269ae3248ee8cd8bae97a503824ad2faf0ece
Matt Johnston <emollientspy1@gmail.com>
2021-03-24 09:01:01 -0700

    seeding

--- app/seeds/steps.py
+++ app/seeds/steps.py
@@ -4,6 +4,14 @@ from app.models import db, Step
 def seed_steps():
     step1 = Step(step_count=1, project_id=1, step_img="img-url",
                  step="This is the first step to my project")
+    step2 = Step(step_count=1, project_id=1, step_img="img-url",
+                 step="This is the first step to my project")
+    step3 = Step(step_count=1, project_id=1, step_img="img-url",
+                 step="This is the first step to my project")
+    step4 = Step(step_count=1, project_id=1, step_img="img-url",
+                 step="This is the first step to my project")
+    step5 = Step(step_count=1, project_id=1, step_img="img-url",
+                 step="This is the first step to my project")
 
     db.session.add(step1)
 
