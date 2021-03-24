3d8264c5278c8bc9f1a1e9d5230888416d115dd9
Matt Johnston <emollientspy1@gmail.com>
2021-03-24 10:02:15 -0700

    in progress

--- app/api/api_routes.py
+++ app/api/api_routes.py
@@ -59,7 +59,7 @@ def api_create_project(userId, categoryId):
 def api_create_one_step(projectId):
     data = request.get_json()
     step = Step(step_count=data['step_count'],
-                project_id=projectId, step_img=data['step_img'], step=data['step'])
+                project_id=projectId, step_imgs=data['step_imgs'], step=data['step'])
     db.session.add(step)
     db.session.commit()
     return step.to_dict()
@@ -74,7 +74,7 @@ def api_create_all_steps(projectId):
     for i in range(len(steps)):
         currentStep = steps[i]
         step = Step(step_count=currentStep['step_count'],
-                    project_id=projectId, step_img=currentStep['step_img'], step=currentStep['step'])
+                    project_id=projectId, step_imgs=currentStep['step_imgs'], step=currentStep['step'])
         db.session.add(step)
     db.session.commit()
     result = db.session.query(Step).filter_by(project_id=projectId)
--- app/seeds/steps.py
+++ app/seeds/steps.py
@@ -2,18 +2,22 @@ from app.models import db, Step
 
 
 def seed_steps():
-    step1 = Step(step_count=1, project_id=1, step_img="img-url",
+    step1 = Step(step_count=1, project_id=1, step_imgs=['asdfa', 'aaaa'],
                  step="This is the first step to my project")
-    step2 = Step(step_count=1, project_id=1, step_img="img-url",
-                 step="This is the first step to my project")
-    step3 = Step(step_count=1, project_id=1, step_img="img-url",
-                 step="This is the first step to my project")
-    step4 = Step(step_count=1, project_id=1, step_img="img-url",
-                 step="This is the first step to my project")
-    step5 = Step(step_count=1, project_id=1, step_img="img-url",
-                 step="This is the first step to my project")
+    step2 = Step(step_count=2, project_id=1, step_imgs=['asdgasd', 'asdaf'],
+                 step="This is the second step to my project")
+    step3 = Step(step_count=3, project_id=1, step_imgs=['asgdasdg', 'asdfasdf'],
+                 step="This is the third step to my project")
+    step4 = Step(step_count=4, project_id=1, step_imgs=['asdgasd', 'asfa'],
+                 step="This is the fourth step to my project")
+    step5 = Step(step_count=5, project_id=1, step_imgs=['asdasgd', 'asdg'],
+                 step="This is the fifth step to my project")
 
     db.session.add(step1)
+    db.session.add(step2)
+    db.session.add(step3)
+    db.session.add(step4)
+    db.session.add(step5)
 
     db.session.commit()
 
