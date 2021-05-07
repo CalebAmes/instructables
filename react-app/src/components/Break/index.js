a470498ffe49796950ce7ef4d43e07e7cdd7def8
emollientspy1 <emollientspy1@gmail.com>
2021-05-06 19:35:36 -0600

    spacing

--- react-app/src/components/Break/index.js
+++ react-app/src/components/Break/index.js
@@ -6,7 +6,6 @@ export function CommentBreak() {
     <div className="commment-break">________________________________________________________________________________________</div>
   )
 }
-
 export function StepBreak() {
   return (
     <div className="step-break">________________</div>
